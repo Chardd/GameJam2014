1deb546fa4bfe597b0e7b3b075be4bb7bf23f093
Chardd <addictedtocurry@hotmail.com>
2014-12-15 21:53:17 -0800

    Added Player and Room
    Added the a Player object with playholder sprite.
    Added first Room with some test tiles.

---
+++ GameJamProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	//Variables used for Player sprite Animation
+	private Rigidbody2D playerBody;
+	private Animator anim;
+	public float speed; //Movement Speed
+
+
+	// Use this for initialization
+	void Start () {
+		playerBody = this.rigidbody2D;
+		anim = GetComponent<Animator>();
+		speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveDirHor = Input.GetAxisRaw ("Horizontal"); // Get Left/Right movement
+		float moveDirVer = Input.GetAxisRaw ("Vertical"); //Get Up/Down movement
+		playerBody.velocity = new Vector2 (moveDirHor * speed, moveDirVer * speed);
+	}
+}
