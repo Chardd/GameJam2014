fe8e971e9f2b22681650300bf41f218ad410e387
Chardd <addictedtocurry@hotmail.com>
2014-12-19 18:22:30 -0800

    Added new enemy sprite
    Added new enemy sprite, framework for animation is there, but logic is
    still incorrect.

--- GameJamProject/Assets/Scripts/EnemyScript.cs
+++ GameJamProject/Assets/Scripts/EnemyScript.cs
@@ -16,12 +16,16 @@ public class EnemyScript : MonoBehaviour {
 	public bool recovering = false;
 	// Used for sprite animation transition
 	private Animator anim;
+	private float vert;
+	private float horz;
 
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindWithTag("Player").transform;
 		anim = GetComponent<Animator>();
+		vert = enemy.position.y;
+		horz = enemy.position.x;
 	}
 	
 	// Update is called once per frame
@@ -36,6 +40,39 @@ public class EnemyScript : MonoBehaviour {
 			if (Vector3.Distance (target.position, enemy.position) < triggerDistance) {
 					enemy.position = Vector3.MoveTowards (enemy.position, target.position, speed * Time.deltaTime);
 					//Debug.Log("DETECT");
+				//Animation handling maths
+				float curVert = enemy.position.y;
+				float curHorz = enemy.position.x;
+				//float diffVert = vert - curVert;
+				//float diffHorz = horz - curHorz;
+				//Debug.Log (diffVert);
+				//Debug.Log (diffHorz);
+				if (curVert == vert)
+				{
+					anim.SetFloat ("Vertical", 0);
+				}
+				if (curVert > vert)
+				{
+					anim.SetFloat ("Vertical", 1);
+				}
+				if (curVert < vert)
+				{
+					anim.SetFloat ("Vertical", -1);
+				}
+				if (curHorz == horz)
+				{
+					anim.SetFloat ("Horizontal", 0);
+				}
+				if (curHorz > horz)
+				{
+					anim.SetFloat ("Horizontal", 1);
+				}
+				if (curHorz < horz)
+				{
+					anim.SetFloat ("Horizontal", -1);
+				}
+				vert = curVert;
+				horz = curHorz;
 			} else {
 					//Debug.Log("LOITER");
 			}
