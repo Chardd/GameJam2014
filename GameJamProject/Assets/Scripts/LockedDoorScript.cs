c1820bc9c51421f23cd8400525e1072c1e67af67
Chardd <addictedtocurry@hotmail.com>
2014-12-20 09:02:44 -0800

    Added locked door, ending screen, fixed UI bugs
    Added a door that checks for key, if player has it will go to a "Win"
    splash screen, with offer to hit "Space" to play again. Fixed issues
    with the health bar visibility.

---
+++ GameJamProject/Assets/Scripts/LockedDoorScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedDoorScript : MonoBehaviour {
+	public float transitionDuration = 2.5f; 
+	
+	public Transform camera;
+	public Transform player;
+	GameObject playerObj;
+	public bool win;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindWithTag("MainCamera").transform;
+		player = GameObject.FindWithTag("Player").transform;
+		playerObj = GameObject.FindWithTag("Player");
+		win = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (win) 
+		{
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				Application.LoadLevel (0);
+			}
+		}
+	}
+	
+	// If player collides with the door
+	void OnTriggerEnter2D(Collider2D collider){
+
+		if (collider.gameObject.tag == "Player"){
+			if(playerObj.GetComponent<PlayerScript>().hasKey == true)
+			{
+				playerObj.GetComponent<PlayerScript>().started = false;
+				playerObj.GetComponent<PlayerScript>().keyDisplay.enabled = false;
+				playerObj.GetComponent<PlayerScript>().upgradeDisplay.enabled = false;
+				GameObject.FindWithTag ("Canvas").SetActive(false);
+				camera.position = new Vector3(58.63f,25.48f,-10.0f);
+				win = true;
+
+			}
+		}
+	}
+}
